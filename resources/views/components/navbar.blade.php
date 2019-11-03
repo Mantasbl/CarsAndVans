eadd689e175566d9a89d76dfaa2ecd815d07cf98
Mantasbl <mantas.baltakis@gmail.com>
2019-11-03 20:37:17 +0000

    Final touches, updated readme

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<div class="nav-container container">
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="/">
+            <img src="{{asset('assets/public_site_images/logo.svg')}}" height="40" class="d-inline-block align-top"
+                alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/search">Store</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
